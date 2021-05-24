f0a291e512b17506cadacd6b759e6e64e29e4f68
Alex Jones <amjones@live.co.uk>
2021-05-24 10:53:02 +0100

    Adds file for creatining Overwatch patch scraper class

---
+++ bot_code/patch_scraper.py
@@ -0,0 +1,160 @@
+"""
+...
+"""
+
+# Standard library imports
+import os
+import requests
+
+# Third party imorts
+from bs4 import BeautifulSoup
+
+class Overwatch_Patch_Scraper():
+    """
+    ...
+    """
+
+    def __init__(self):
+        """
+        ...
+        """
+        self.live_patches_url = 'https://playoverwatch.com/en-us/news/patch-notes/live'
+        self.experimental_patches_url = 'https://playoverwatch.com/en-us/news/patch-notes/experimental'
+        self.latest_live_patch = self.get_latest_patch(self.live_patches_url)
+        # Create a patch date file if we don't already have one
+        if not os.path.exists(".livepatchdate"):
+            with open(".livepatchdate", "w") as f:
+                f.write(self.get_patch_date(self.latest_patch))
+
+
+    def get_latest_patch(self, url: str):
+        """
+        ...
+        """
+        latest_patch = self.get_patch_i(url, 0)
+        self.latest_patch = latest_patch
+        return latest_patch
+
+    
+    def check_patch_date(self):
+        """
+        ...
+        bs4.element.Tag
+        """
+        patch_date = self.get_patch_date(self.latest_live_patch)
+        with open(".livepatchdate", "r") as f:
+            old_patch_date = f.read()
+        
+        new_patch = True if old_patch_date != patch_date else False
+        if new_patch:
+            with open(".livepatchdate", "w") as f:
+                f.write(patch_date)
+
+        return new_patch
+
+
+    def write_patch_notes(self, patch):
+        """
+        ...
+        """
+        patch_note_string = f"A new Overwatch patch has been released! Patch notes from: {self.__get_patch_date(patch)}:\n\n"
+        patch_sections = patch.find_all("div", class_="PatchNotes-section-generic_update")
+        first_section_title = True
+
+        for section in patch_sections:
+            # Some patch notes have a section title - add this to the string in bold
+            section_title = section.find("h4", class_="PatchNotes-sectionTitle")
+            if section_title:
+                # Add new lines to subsequent section titles
+                if first_section_title:
+                    first_section_title = False
+                else:
+                    patch_note_string += "\n\n"
+                patch_note_string += f"__**{section_title.get_text()}**__"
+                       
+            # Get the title and notes from the section of patches
+            patch_titles = section.find_all("div", class_="PatchNotesGeneralUpdate-title")
+            patch_notes = section.find_all("div", class_="PatchNotesGeneralUpdate-description")
+
+            # Loop through patches for the given section and add titles and patches to the patch_note_string
+            for i, patch_note in enumerate(patch_notes):
+                try:
+                    patch_title = patch_titles[i].get_text()
+                    patch_note_string += f"\n\n**{patch_title}**\n"
+                except IndexError:
+                # No patch_title for events and possibly other patches, so just add new line here
+                    patch_note_string += "\n"
+                # Prettify nested patch notes by making maps/characters (a single word) italics on a new line
+                patch_note_lines = patch_note.get_text().strip().split('\n\n')
+                for i, line in enumerate(patch_note_lines):
+                    line = line.strip()
+                    if not line:
+                        continue
+                    elif len(line.split(" ")) == 1:
+                        patch_note_text = "\n*" + line + "*"
+                        patch_note_text = "\n" + patch_note_text if i > 1 else patch_note_text
+                    else:
+                        patch_note_text = "\n" + line
+                    patch_note_string += f"{patch_note_text}"
+        
+        return patch_note_string
+
+
+    def create_messages(self, patch_note_string):
+        """
+        ...
+        """
+        messages = []
+        done = False
+        patch_segment, second_patch_half = patch_note_string, ""
+        while done == False:
+            patch_segment, second_patch_segment = self.__split_message(patch_segment, second_patch_segment)
+            messages.append(patch_segment)
+            if len(second_patch_segment) < 2000:
+                messages.append(second_patch_segment)
+                done = True
+        return messages
+
+
+    def __get_patch_date(self, patch):
+        """
+        ...
+        """
+        patch_date = patch.find("div", class_="PatchNotes-date").get_text()
+        return patch_date
+    
+
+    def __get_patch_i(self, url: str, i: int):
+        """
+        ...
+        """
+        response = requests.get(url)
+        patches_page = BeautifulSoup(response.text, 'html.parser')
+        patch = patches_page.find_all("div", class_="PatchNotes-patch")[i]
+        return patch
+
+
+    def __split_message(patch_segment, second_patch_segment):
+        """
+        ...
+        """
+        if len(patch_segment) > 2000:
+            patch_section_index = patch_segment.rindex("__**", 0, 2000)
+            second_patch_segment = patch_segment[patch_section_index:]
+            patch_segment = patch_segment[:patch_section_index]
+        else:
+            second_patch_segment = ""
+        return patch_segment, second_patch_segment
+
+
+
+
+
+if __name__ == "__main__":
+    scraper = Overwatch_Patch_Scraper()
+    scraper.get_latest_patch(scraper.live_patches_url)
+    #patch_note_string = scraper.write_patch_notes(scraper.latest_patch)
+    #messages = create_messages(patch_note_string)
+    #for m in messages:
+    #    print(m)
+    #    print("NEW MESSAGE")
